6c092209027fc9bbb6379896f0469f61ae5a085f
Luis Vilella <luivilella@gmail.com>
2019-02-07 07:38:06 +0000

    feat(py project): add psql sample
    Adding a sample of how to use psql.

--- application/server.py
+++ application/server.py
@@ -1,9 +1,44 @@
+import os
+
 import bottle
+import psycopg2
+
+
+def get_db_active_users() -> list:
+    """Function to get all active users from database
+
+    Returns:
+        list: list of strings with user's name
+
+    Examples:
+        >>> print(get_db_active_users())
+        ['user1', 'user2']
+    """
+
+    conn = psycopg2.connect(
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        host=os.getenv('DB_HOST'),
+        password=os.getenv('DB_PASSWORD'),
+    )
+
+    cur = conn.cursor()
+    sql = '''
+        SELECT
+            DISTINCT(usename) AS usename
+        FROM pg_stat_activity
+        WHERE
+            state = 'active'
+    '''
+    cur.execute(sql)
+    rows = cur.fetchall()
+    return [usename for usename, in rows]
 
 
 @bottle.route('/')
 def index():
-    return 'Running inside docker :)'
+    active_users = get_db_active_users()
+    return f'Active users: {active_users}'
 
 
 if __name__ == '__main__':
